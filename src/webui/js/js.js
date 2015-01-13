90e98011cc853667a7676990c1298944d42bb473
Wenyu Zhou <super-giraffe@qq.com>
2015-01-13 20:16:40 +0800

    Revert 92bd93c..8e18641
    This rolls back to commit 92bd93c2eb329e2dba7ed80afc48004b8c8c078f.

---
+++ src/webui/js/js.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+var nav;
+    function over1(){
+        document.getElementById("rx-top-person-menu").style.display = "block";clearTimeout(nav);
+    }
+    function out1(){
+        nav = setTimeout(hide,100);
+    }
+    function hide(){
+        document.getElementById("rx-top-person-menu").style.display = "none"
+    }
+</script>
--- src/webui/readme.markdown
+++ src/webui/readme.markdown
@@ -1,1 +1,2 @@
 This is a placeholder/readme for frontend code.
+
