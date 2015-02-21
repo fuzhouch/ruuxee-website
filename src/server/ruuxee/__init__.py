c4466c7ec50868c62e4a44da165572d0cfafff04
fuzhou.chen <fuzhou.chen@ruuxee.com>
2015-02-21 11:24:03 +0800

    [DOC][SERVER] Add document for Application.

--- src/server/ruuxee/__init__.py
+++ src/server/ruuxee/__init__.py
@@ -16,7 +16,23 @@ else:
 APP_NAME = 'ruuxee' # Hardcoded package name. Don't change.
 
 class Application(flask.Flask):
-    "Toplevel flask application generator."
+    """
+    Toplevel flask application generator.
+
+    This is the main entry point of ruuxee Application. It can be used
+    directly.
+
+    A quick example:
+        >>> import ruuxee
+        >>> import ruuxee.apis.v1.web
+        >>> import ruuxee.views.person
+        >>> app = ruuxee.Application(config='ruuxee.config.webui_dev')
+        >>> api_page = ruuxee.apis.v1.web.page
+        >>> person_page = ruuxee.views.person.page
+        >>> app.register_blueprint(api_page, url_prefix='/api/web/v1')
+        >>> app.register_blueprint(person_page, url_prefix='/person')
+        >>> app.run(debug=True)
+    """
     def __init__(self, config, template_folder=None):
         """Application.__init__(self, config)
 
@@ -43,7 +59,13 @@ class Application(flask.Flask):
         return flask.current_app.config["RUUXEE_SESSION_MANAGER"]
 
 class View(flask.Blueprint):
-    "Toplevel view generator, based on :object:flask.Blueprint."
+    """
+    Toplevel view generator, based on :object:flask.Blueprint.
+
+    Please note that View is not a directly usable class. Instead, it's
+    a simple wrapper to flask.Blueprint, which must be extended to
+    implement its own features.
+    """
     def __init__(self, view_name):
         # Make sure all views share the same inherited folder structure.
         self.__app_name = APP_NAME
